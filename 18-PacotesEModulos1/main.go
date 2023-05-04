8dc2a554886e464bd5271efd1344a9aa1edfea8e
renatorv <renatorv@gmail.com>
2023-05-04 08:38:19 -0300

    Finalizada aula sobre o FOR

--- 18-PacotesEModulos1/main.go
+++ 18-PacotesEModulos1/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/renatorv/gordin/matematica"
+
+	"github.com/google/uuid"
 )
 
 type MyNumber int
@@ -13,9 +15,22 @@ func main() {
 
 	fmt.Println("Resultado:", res)
 
+	fmt.Println(matematica.A)
+
+	carro := matematica.Carro{Marca: "Volkwagem"}
+	fmt.Println(carro.Marca)
+	fmt.Println(carro.Andar())
+
+	fmt.Println(uuid.New())
+
 }
 
 // go mod init <nome_modulo>
 // go mod init github.com/renatorv/gordin
 // nome_modulo: sugestão endereço do github
 // github.com/renatorv/curso-go
+
+// INSTALANDO MÓDULOS
+// go get github.com/google/uuid
+// ou
+// github.com/google/uuid no import e depois: go mod tidy no terminal
--- 18-PacotesEModulos1/matematica/matematica.go
+++ 18-PacotesEModulos1/matematica/matematica.go
@@ -3,3 +3,17 @@ package matematica
 func Soma[T int | float64](a, b T) T {
 	return a + b
 }
+
+var A int = 10
+
+type Carro struct {
+	Marca string
+}
+
+func (c Carro) Andar() string {
+	return "Carro andando..."
+}
+
+// Caso altere o nome da função, variável ou struct para minúsculo,
+// soma, a ou carro [marcar], esses parâmetros não serão encontrados
+// fora desse módulo!!!!!!
